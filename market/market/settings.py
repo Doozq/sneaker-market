65271fe30c80929b0c5ad0a2ba8df09d9d01eedc
Симионов Андрей Владимирович <avsimionov@edu.hse.ru>
2024-05-28 02:31:33 +0300

    Все

--- market/market/settings.py
+++ market/market/settings.py
@@ -122,13 +122,16 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_ROOT = "static"
-STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "static_dev",
-]
+STATIC_URL = "/static/"
+
+MEDIA_URL = "/media/"
+
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-MEDIA_ROOT = BASE_DIR / "media"
-MEDIA_URL = "/media/"
